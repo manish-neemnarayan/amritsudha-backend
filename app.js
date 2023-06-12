1fd6ec5d2692fa8b2bf46be9efc192de40a89a72
manish-neemnarayan <manish9301kumar@gmail.com>
2023-06-12 11:01:11 +0530

    cors origin

--- app.js
+++ app.js
@@ -10,11 +10,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
-// app.use(cors({ origin: true, credentials: true }));
-app.use(cors({
-    origin: 'https://amritsudha-frontend.vercel.app',
-    credentials: true
-}));
+app.use(cors({ origin: '*', credentials: true }));
+// app.use(cors());
 // Enable CORS
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*'); // Allow requests from all origins
