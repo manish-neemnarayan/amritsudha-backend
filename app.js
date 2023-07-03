c6b842788dcaf8ab0a19e4d084fba914f5b8a845
manish-neemnarayan <manish9301kumar@gmail.com>
2023-07-03 08:40:25 +0530

    minor change in app.js

--- app.js
+++ app.js
@@ -9,19 +9,21 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(cookieParser());
 app.use(cors({
   origin: "http://localhost:3000", // Replace with the actual origin of your React application
   credentials: true // Allow including credentials (cookies)
 }));
 // app.use(cors());
-// Enable CORS
-app.use((_req, res, next) => {
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Allow the specified HTTP methods
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-  });
+
+app.use(cookieParser());
 app.use(morgan('tiny'));
 app.use(router);
+
+// Enable CORS
+app.use((_req, res, next) => {
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Allow the specified HTTP methods
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 module.exports = app;
