31f89ae3c8df22373a0c67fbd77f16af4bf543c1
manish-neemnarayan <manish9301kumar@gmail.com>
2023-07-03 08:16:56 +0530

    httponly:false

--- controller/auth.js
+++ controller/auth.js
@@ -7,7 +7,7 @@ const mailHelper = require("../util/mailHelper");
 // cookie options
 const cookieOptions = {
     // expiresIn: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000 ),
-        httpOnly: true,
+        httpOnly: false,
         sameSite: 'None',
         secure: false,
         domain: '.onrender.com',
