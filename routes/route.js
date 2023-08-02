8f091980594cb92910ff33977f69d8f43c8d4fc4
manish-neemnarayan <manish9301kumar@gmail.com>
2023-08-02 19:04:59 +0530

    swagger docs added

--- routes/route.js
+++ routes/route.js
@@ -10,37 +10,357 @@ const {isMember} = require("../middleware/auth");
 const Router = express.Router();
 
 
-Router.get('/health', (req, res) => {
+Router.get('/health', (_req, res) => {
     res.status(200).json({ status: 'OK' });
   });
 // authentication routes
+
+/**
+ * @swagger
+ * /api/auth/signup:
+ *   post:
+ *     description: for creating a new user
+ *     responses:
+ *       200:
+ *         description: created user
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/model/authSchema'
+ */
 Router.post("/api/auth/signup", isLoggedIn, isAdmin, signup);
+
+/**
+ * @swagger
+ * /api/auth/login:
+ *   post:
+ *     description: to login a user and return user object
+ *     responses:
+ *       200:
+ *         description: success: true, user
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/model/authSchema'
+ */
 Router.post("/api/auth/login", login);
+
+/**
+ * @swagger
+ * /api/auth/logout:
+ *   post:
+ *     description: to logout user
+ *     responses:
+ *       200:
+ *         description: clearing cookies 
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/model/authSchema'
+ */
 Router.post("/api/auth/logout",isLoggedIn, logout);
+
+/**
+ * @swagger
+ * /api/auth/user/update/:userId:
+ *   put:
+ *     description: Update the user into the database
+ *     responses:
+ *       200:
+ *         description: return updated user
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/model/authSchema'
+ */
 Router.put("/api/auth/user/update/:userId", isLoggedIn, isAdmin, updateUser);
+
+/**
+ * @swagger
+ * /api/auth/getAllUser:
+ *   get:
+ *     description: Retrieve all signed up user
+ *     responses:
+ *       200:
+ *         description: return users
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/model/authSchema'
+ */
 Router.get("/api/auth/getAllUser", isLoggedIn, isMember, getAllUser);
+
+/**
+ * @swagger
+ * /api/auth/getUser/:userId:
+ *   get:
+ *     description: Retrieve login user
+ *     responses:
+ *       200:
+ *         description: return loggedin user
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/model/authSchema'
+ */
 Router.get("/api/auth/getUser/:userId", isLoggedIn, isMember, getUser);
+
+/**
+ * @swagger
+ * /api/auth/getUser/:userId:
+ *   delete:
+ *     description: delete user by specified id
+ *     responses:
+ *       200:
+ *         description: User is removed successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/model/authSchema'
+ */
 Router.delete("/api/auth/user/delete/:userId", isLoggedIn, isAdmin, deleteUser);
 
 
 // gallery routes
+
+/**
+ * @swagger
+ * /api/image/upload:
+ *   post:
+ *     description: upload the image using cloudinary
+ *     responses:
+ *       200:
+ *         description: upload the image
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/model/gallerySchema'
+ */
 Router.post("/api/image/upload", isLoggedIn, isAdmin, upload.single("image"), uploadImage)
+
+/**
+ * @swagger
+ * /api/image/delete/:image_id:
+ *   delete:
+ *     description: delete the image from db and cloudinary
+ *     responses:
+ *       200:
+ *         description: upload the image
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/model/gallerySchema'
+ */
 Router.delete("/api/image/delete/:image_id", isLoggedIn, isAdmin, deleteImage);
+
+/**
+ * @swagger
+ * /api/image/getAllImages:
+ *   get:
+ *     description: Getting all the images
+ *     responses:
+ *       200:
+ *         description: Getting all the images
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/model/gallerySchema'
+ */
 Router.get("/api/image/getAllImages", getImages);
 
 
 // notice routes
+
+/**
+ * @swagger
+ * /api/notice/create:
+ *   post:
+ *     description:create a new notice and return created notice
+ *     responses:
+ *       200:
+ *         description: user should be admin 
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/model/noticeSchema'
+ */
 Router.post("/api/notice/create", isLoggedIn, isAdmin, createNotice);
+
+/**
+ * @swagger
+ * /api/notice/update/:noticeId:
+ *   put:
+ *     description: update a notice in database
+ *     responses:
+ *       200:
+ *         description: user should be admin 
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/model/noticeSchema'
+ */
 Router.put("/api/notice/update/:noticeId", isLoggedIn, isAdmin, updateNotice);
+
+/**
+ * @swagger
+ * /api/notice/get/:noticeId:
+ *   get:
+ *     description: Get a notice from database
+ *     responses:
+ *       200:
+ *         description: ""
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/model/noticeSchema'
+ */
 Router.get("/api/notice/get/:noticeId", isLoggedIn, isAdmin, getNotice);
+
+/**
+ * @swagger
+ * /api/notice/getAll:
+ *   get:
+ *     description: Get all notices from database
+ *     responses:
+ *       200:
+ *         description: ""
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/model/noticeSchema'
+ */
 Router.get("/api/notice/getAll", isLoggedIn, isMember, getAllNotice);
+
+/**
+ * @swagger
+ * /api/notice/delete/:noticeId:
+ *   delete:
+ *     description: delete a notice from database
+ *     responses:
+ *       200:
+ *         description: 
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/model/noticeSchema'
+ */
 Router.delete("/api/notice/delete/:noticeId", isLoggedIn, isAdmin, deleteNotice);
 
 // program routes
+
+/**
+ * @swagger
+ * /api/program/create:
+ *   post:
+ *     description: create a new program 
+ *     responses:
+ *       200:
+ *         description: 
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/model/awarenessProgramSchema'
+ */
 Router.post("/api/program/create", isLoggedIn, isAdmin, createProgram);
+
+/**
+ * @swagger
+ * /api/program/update/:programId:
+ *   put:
+ *     description: update an existing program 
+ *     responses:
+ *       200:
+ *         description: return updated notice
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/model/awarenessProgramSchema'
+ */
 Router.put("/api/program/update/:programId", isLoggedIn, isAdmin, updateProgram);
+
+/**
+ * @swagger
+ * /api/program/get/:programId
+ *   get:
+ *     description: get an existing program 
+ *     responses:
+ *       200:
+ *         description: return retreived notice
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/model/awarenessProgramSchema'
+ */
 Router.get("/api/program/get/:programId", isLoggedIn, isAdmin, getProgram);
+
+/**
+ * @swagger
+ * /api/program/getAll
+ *   get:
+ *     description: get an existing program 
+ *     responses:
+ *       200:
+ *         description: return retreived notices
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/model/awarenessProgramSchema'
+ */
 Router.get("/api/program/getAll", getAllProgram);
+
+/**
+ * @swagger
+ * /api/program/getAll
+ *   delete:
+ *     description: delete an existing program 
+ *     responses:
+ *       200:
+ *         description: return deleted notice
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/model/awarenessProgramSchema'
+ */
 Router.delete("/api/program/delete/:programId", isLoggedIn, isAdmin, deleteProgram);
 
 module.exports = Router;
---
+++ swagger.js
@@ -0,0 +1,20 @@
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Amritsudha',
+      version: '1.0.0',
+      description: 'A short description of your API',
+    },
+  },
+  apis: ['./routes/*.js'], // Path to the API docs
+};
+
+const specs = swaggerJsdoc(options);
+
+module.exports = (app) => {
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+};
