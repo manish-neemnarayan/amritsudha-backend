29c33c288e0b23b2e3e32b8e0b34a42a74c1dbe7
manish-neemnarayan <manish9301kumar@gmail.com>
2023-08-03 15:49:23 +0530

    cors origin changed

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors({
-  origin: "*", // Replace with the actual origin of your React application
+  origin: "https://amritsudha-frontend.vercel.app", // Replace with the actual origin of your React application
   credentials: true // Allow including credentials (cookies)
 }));
 // app.use(cors());
@@ -20,7 +20,7 @@ app.use(morgan('tiny'));
 app.use(router);
 // Enable CORS
 app.use((_req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "https://amritsudha-frontend.vercel.app");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   next();
